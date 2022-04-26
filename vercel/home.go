74efe4166be75814fa28dd56fa364b4df32e9ec6
sxyazi <sxyazi@gmail.com>
2022-04-27 02:10:08 +0800

    feat: add refresh key

--- vercel/home.go
+++ vercel/home.go
@@ -2,9 +2,16 @@ package vercel
 
 import (
 	"github.com/sxyazi/bendan/boot"
+	. "github.com/sxyazi/bendan/utils"
 	"net/http"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("key") != Config("refresh_key") {
+		w.WriteHeader(http.StatusFound)
+		w.Header().Set("Location", "https://github.com/sxyazi/bendan")
+		return
+	}
+
 	boot.ServeHook(w, r)
 }
