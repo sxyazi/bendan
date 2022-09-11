45ead913ceb5568b49daa1a80c29e863c0b97561
sxyazi <sxyazi@gmail.com>
2022-09-11 21:46:09 +0800

    feat: ignore non short hosts

--- commands/purify/short.go
+++ commands/purify/short.go
@@ -4,14 +4,23 @@ import (
 	"github.com/sxyazi/bendan/utils"
 	"net/url"
 	"regexp"
+	"strings"
 )
 
 type short struct{}
 
-var reShort = regexp.MustCompile(`^https?://[a-zA-Z0-9]{1,5}\.[a-zA-Z0-9]{2,3}/[a-zA-Z0-9_-]{4,8}$`)
+var nonShortHost = []string{
+	`t\.me`,
+}
+
+var reShortUrl = regexp.MustCompile(`^https?://[a-zA-Z0-9]{1,5}\.[a-zA-Z0-9]{2,3}/[a-zA-Z0-9_-]{4,8}$`)
+var reShortNonHost = regexp.MustCompile(`(?i)^(` + strings.Join(nonShortHost, "|") + `)$`)
 
 func (*short) match(u *url.URL) []string {
-	return reShort.FindStringSubmatch(u.String())
+	if reShortNonHost.MatchString(u.Hostname()) {
+		return nil
+	}
+	return reShortUrl.FindStringSubmatch(u.String())
 }
 
 func (*short) handle(s *Stage) string {
