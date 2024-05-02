e6ac7b6cad2232725e2df25b271343974182f294
Cyunrei <cyunrei@gmail.com>
2024-05-02 10:08:38 +0800

    refactor: hush.go

--- commands/hush.go
+++ commands/hush.go
@@ -49,22 +49,19 @@ func writeTimeToFile(chatID string) error {
 
 func Hush(msg *tgbotapi.Message) bool {
 	chatID := strconv.FormatInt(msg.Chat.ID, 10)
+
 	if msg.ReplyToMessage != nil && strings.Contains(msg.Text, "说话") {
-		err := writeTimeToFile(chatID)
-		if err == nil {
-			err := os.Remove(filepath.Join(filepath.Join(hushDir, chatID)))
-			if err != nil {
-				log.Println("Hush Err:", err)
-				ReplyText(msg, "想说，但说不出来...")
-				return true
-			}
+		err := os.Remove(filepath.Join(hushDir, chatID))
+		if err != nil {
+			log.Println("Hush Err:", err)
+			ReplyText(msg, "想说，但说不出来...")
+		} else {
 			ReplyText(msg, "已经在说了...")
-			return true
 		}
+		return true
 	}
 
 	expiredTime, err := readTimeFromFile(chatID)
-
 	if err == nil && time.Now().Before(expiredTime) {
 		return true
 	}
@@ -73,12 +70,12 @@ func Hush(msg *tgbotapi.Message) bool {
 		err := writeTimeToFile(chatID)
 		if err == nil {
 			ReplyText(msg, "好吧...")
-			return true
 		} else {
 			log.Println("Hush Err:", err)
 			ReplyText(msg, "想闭，但闭不了嘴...")
-			return false
 		}
+		return true
 	}
+
 	return false
 }
