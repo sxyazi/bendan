4d3a8312c2b8b727a4575602851898c9cff20bfa
Cyunrei <cyunrei@gmail.com>
2024-05-03 09:33:02 +0800

    refactor: hush.go

--- commands/hush.go
+++ commands/hush.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 	"os"
@@ -22,37 +21,11 @@ func init() {
 	}
 }
 
-func readTimeFromFile(chatID string) (time.Time, error) {
-	content, err := os.ReadFile(filepath.Join(hushDir, chatID))
-	if err != nil {
-		return time.Time{}, fmt.Errorf("readTimeFromFile: %v", err)
-	}
-
-	unixTime, err := strconv.ParseInt(string(content), 10, 64)
-	if err != nil {
-		return time.Time{}, fmt.Errorf("readTimeFromFile: %v", err)
-	}
-
-	return time.Unix(unixTime, 0), nil
-}
-
-func writeTimeToFile(chatID string) error {
-	path := filepath.Join(hushDir, chatID)
-	content := strconv.FormatInt(time.Now().Add(30*time.Minute).Unix(), 10)
-
-	err := os.WriteFile(path, []byte(content), 0644)
-	if err != nil {
-		log.Println("writeTimeToFile: ", err)
-	}
-
-	return err
-}
-
 func Hush(msg *tgbotapi.Message) bool {
-	chatID := strconv.FormatInt(msg.Chat.ID, 10)
+	path := filepath.Join(hushDir, strconv.FormatInt(msg.Chat.ID, 10))
 
 	if msg.ReplyToMessage != nil && reHush.MatchString(msg.Text) {
-		err := writeTimeToFile(chatID)
+		err := os.WriteFile(path, nil, 0644)
 		if err != nil {
 			log.Println("Hush Err:", err)
 			ReplyText(msg, "想闭，但闭不了嘴。。。")
@@ -63,7 +36,6 @@ func Hush(msg *tgbotapi.Message) bool {
 	}
 
 	if msg.ReplyToMessage != nil && reUnHush.MatchString(msg.Text) {
-		path := filepath.Join(hushDir, chatID)
 		if _, err := os.Lstat(path); err == nil {
 			err := os.Remove(path)
 			if err != nil {
@@ -78,8 +50,8 @@ func Hush(msg *tgbotapi.Message) bool {
 		return true
 	}
 
-	expiredTime, err := readTimeFromFile(chatID)
-	if err == nil && time.Now().Before(expiredTime) {
+	info, err := os.Lstat(path)
+	if err == nil && time.Now().Before(info.ModTime().Add(30*time.Minute)) {
 		return true
 	}
 
